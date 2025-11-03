2e7d9ecef8a5b141e18b162988caa4e57f25ff8a
Madra_Ropucha <szymon291203@gmail.com>
2025-11-03 16:20:51 +0100

    fix: the counter for init noe goes from 0 to n

--- p7/myList.ml
+++ p7/myList.ml
@@ -59,9 +59,9 @@ let flatten ll = concat ll;;
 
 let init l f = 
   let rec aux i acc =
-    if i < 0 then rev acc
-    else aux (i - 1) (f i :: acc)
-  in aux (l - 1) [];;
+    if i >= l then rev acc
+    else aux (i + 1) (f i :: acc)
+  in aux (0) [];;
 
 let nth l n = 
   if n < 0 then raise (Failure "nth")

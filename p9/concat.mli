ffcf674c9bdd4c51b3cd1287225a9d6bb41bc318
Madra_Ropucha <szymon291203@gmail.com>
2025-11-24 16:30:54 +0100

    added files

---
+++ p9/concat.mli
@@ -0,0 +1,4 @@
+val concat' : 'a list list -> 'a list
+val concat'' : 'a list list -> 'a list
+val sublists : 'a list -> 'a list list
+
---
+++ p9/tail.mli
@@ -0,0 +1,4 @@
+val front : 'a list -> 'a list
+val compress : 'a list -> 'a list
+val fold_right : ('a -> 'b -> 'b) -> 'a list -> 'b -> 'b
+

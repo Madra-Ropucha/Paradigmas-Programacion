cb2d777f8b708b10372402fe49acd77ab0c1a365
Madra_Ropucha <szymon291203@gmail.com>
2025-11-24 17:05:06 +0100

    feat: added concat and sublists functions

---
+++ p9/concat.ml
@@ -0,0 +1,26 @@
+(*let concat l =
+  let rec aux acc = function
+    [] -> acc
+    | h::t -> aux (List.append acc h) t
+  in aux [] l*)
+(*Por cada interacion, la funcion recorre acc y acc va creciendo conforme va pasando por cada elemento haciendo asi que sea muy lenta para listas grandes*)
+
+(*let concat' l =
+  List.fold_left (fun acc h -> List.append acc h) [] l*)
+(*Por cada interacion, la funcion recorre acc y acc va creciendo conforme va pasando por cada elemento haciendo asi que sea muy lenta para listas grandes*)
+
+let concat' l = 
+  List.fold_right (fun h acc -> List.append h acc) l [];;
+
+let concat'' l = 
+  let rec aux acc = function
+    []     -> acc
+    | h::t -> match List.rev h with
+                []     -> aux acc t
+              | h'::t' -> aux (h'::acc) (t'::t)
+  in List.rev (aux [] l);;
+
+let sublists l =
+  List.fold_left (fun acc h ->
+    acc @ (List.map (fun sub -> sub @ [h]) acc)
+  ) [[]] l;;

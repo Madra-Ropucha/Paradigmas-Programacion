2c481273332cfea4d8f31792d0b3da2962ce4e16
Madra_Ropucha <szymon291203@gmail.com>
2025-12-01 17:04:20 +0100

    fix: funcionts from optional p9 to actually use tailModCons

--- p9/tail2.ml
+++ p9/tail2.ml
@@ -1,17 +1,17 @@
-let front l =
-  let rec aux acc = function
-    | [] | _::[] -> List.rev acc
-    | h::t       -> aux (h::acc) t
-  in aux [] l;;
+let[@tail_mod_cons] rec front = function
+[] -> raise (Failure "front")
+| h::[] -> []
+| h::t -> h :: front t;;
 
-let compress l =
-  let rec aux acc = function
-    | []        -> List.rev acc
-    | [h]       -> List.rev (h::acc)
-    | h1::h2::t ->
-        if h1 = h2 then aux acc (h2::t)
-        else aux (h1::acc) (h2::t)
-  in aux [] l;;
+let[@tail_mod_cons] rec compress = function
+h1::h2::t ->
+if h1 = h2 then compress (h2::t)
+else h1 :: compress (h2::t)
+| l -> l;;
+
+(*let[@tail_mod_cons] fold_right =
+List.fold_right*)
+(* Tail Modulo Constructor no se puede aplicar en fold_righy, ya que no es una funcion recursiva y tampoco puede usarse dentro de otras fuciones que se llaman en la funcion aplicada, es decir Tail Modulo Constructor no puede modificar fold_right internamente*)
 
   (* TODO: COMPARAR ESTAS DEFINICIONES CON LAS DEL EJERCICIO ANTERIOR 
   MIRAR SU RENDIMIENTO SOBRE LISTAS LARGAS Y ESCRIBIR CONCLUSIONES*)
